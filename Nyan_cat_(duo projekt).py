40e01e175abf8841d13946ff440171175bd90d71
lk_07 <lukakrzan007@gmail.com>
2026-03-12 20:12:09 +0100

    Slika macke

--- Nyan_cat_(duo projekt).py
+++ Nyan_cat_(duo projekt).py
@@ -8,9 +8,20 @@ screen = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Nyan Cat')
 exit = False
 
+slika_macke = pygame.image.load('Nyan_cat_slika.png').convert_alpha()
+
+macka  = slika_macke.get_rect()
+macka.center = (200, 300)
+
 while not exit:
+    screen.fill((0, 0, 0))
+
+    screen.blit(slika_macke, macka)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
 
     pygame.display.update()
+
+pygame.quit()
