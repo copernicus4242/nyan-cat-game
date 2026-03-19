3ad7a45c0afdc093ccbd0e1382c6e52111bd4d2e
copernicus42424 <ghostintheshellzb@gmail.com>
2026-03-19 19:43:43 +0100

    update

--- nyan cat game.py
+++ nyan cat game.py
@@ -1,8 +1,9 @@
 import pygame
+import random
 pygame.init()
 
 window_height = 800
-window_width = 800
+window_width = 1200
 screen = pygame.display.set_mode((window_width, window_height))
 
 pygame.display.set_caption('Nyan Cat')
@@ -13,26 +14,37 @@ print("press r to restart")
 print("press q to quit")
 
 slika_macke = pygame.image.load('Nyan_cat_slika.png').convert_alpha()
+slika_macke = pygame.transform.scale(slika_macke, (200, 100))
 macka  = slika_macke.get_rect()
+platform = pygame.rect.Rect(100, 100, 500, 50)
 velocity_y = 0
+velocity_x = -5
 gravity = 1
 jump_strength = -20
+platform.y = random.randrange(window_height)
 def gravitacija():
     global velocity_y
     velocity_y += gravity
     macka.y += velocity_y
-
+def platforme_premik():
+    global velocity_y
+    platform.x += velocity_x
 def jump():
     global velocity_y
     velocity_y = jump_strength
 
-def screen_collision():
+def collisions():
     if macka.bottom > window_height:
         macka.bottom = window_height
-
+    if macka.colliderect(platform) == True:
+        macka.y -= velocity_y
+    if platform.x < -200:
+        platform.x = 1200
+        platform.y = random.randrange(window_height)
+def spawn_platform():
+    return
 
 def game_loop():
-    macka.center = (200, 300)
     velocity_y = 0
 
     while True:
@@ -51,14 +63,15 @@ def game_loop():
                     return
 
         screen.fill((255, 255, 255))
+        screen.blit(slika_macke, macka)
 
         gravitacija()
-        screen_collision()
+        collisions()
+        platforme_premik()
 
-        pygame.draw.rect(screen, , macka)
-
+        pygame.draw.rect(screen, "black", platform)
         pygame.display.update()
         clock.tick(60)
-
+spawn_platform()
 while True:
     game_loop()
