79d54827f345ed6872be02bcccff0a22b163bb33
copernicus42424 <ghostintheshellzb@gmail.com>
2026-03-26 18:44:58 +0100

    dodal sem vec platform

--- nyan cat game.py
+++ nyan cat game.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+
 pygame.init()
 
 window_height = 800
@@ -13,38 +14,67 @@ print("press SPACE to jump")
 print("press r to restart")
 print("press q to quit")
 
+
 slika_macke = pygame.image.load('Nyan_cat_slika.png').convert_alpha()
 slika_macke = pygame.transform.scale(slika_macke, (200, 100))
-macka  = slika_macke.get_rect()
-platform = pygame.rect.Rect(100, 100, 500, 50)
+macka = slika_macke.get_rect()
+macka.x = 200
+macka.y = 300
+
+
 velocity_y = 0
-velocity_x = -5
 gravity = 1
 jump_strength = -20
-platform.y = random.randrange(window_height)
+velocity_x = -5
+
+platform_list = []
+for i in range(4):
+    platform = pygame.Rect(random.randrange(300, 1200), random.randrange(50, 700), 500, 50)
+    platform_list.append(platform)
+
+
 def gravitacija():
     global velocity_y
     velocity_y += gravity
     macka.y += velocity_y
+
+
 def platforme_premik():
-    global velocity_y
-    platform.x += velocity_x
+    for rect in platform_list:
+        rect.x += velocity_x
+
+
 def jump():
     global velocity_y
     velocity_y = jump_strength
 
+
 def collisions():
+    global velocity_y
+
     if macka.bottom > window_height:
         macka.bottom = window_height
-    if macka.colliderect(platform) == True:
-        macka.y -= velocity_y
-    if platform.x < -200:
-        platform.x = 1200
-        platform.y = random.randrange(window_height)
-def spawn_platform():
-    return
+        velocity_y = 0
+
+    for platform in platform_list:
+        if macka.colliderect(platform) and velocity_y > 0:
+            macka.bottom = platform.top
+            velocity_y = 0
+
+    for platform in platform_list:
+        if platform.x < -500:
+            platform.x = 1200
+            platform.y = random.randrange(50, 700)
+
+
+def draw_every_platform():
+    for rect in platform_list:
+        pygame.draw.rect(screen, "black", rect)
+
 
 def game_loop():
+    global velocity_y
+
     velocity_y = 0
 
     while True:
@@ -63,15 +93,17 @@ def game_loop():
                     return
 
         screen.fill((255, 255, 255))
-        screen.blit(slika_macke, macka)
 
         gravitacija()
         collisions()
         platforme_premik()
 
-        pygame.draw.rect(screen, "black", platform)
+        screen.blit(slika_macke, macka)
+        draw_every_platform()
+
         pygame.display.update()
         clock.tick(60)
-spawn_platform()
+
+
 while True:
     game_loop()
