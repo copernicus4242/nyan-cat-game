e878b1f786d7329f965b7041a523a7087191456d
copernicus42424 <ghostintheshellzb@gmail.com>
2026-04-02 20:13:57 +0200

    dodal tvojo musko, difficulty in main menu

--- nyan cat game.py
+++ nyan cat game.py
@@ -6,17 +6,23 @@ pygame.init()
 window_height = 800
 window_width = 1200
 screen = pygame.display.set_mode((window_width, window_height))
-
 pygame.display.set_caption('Nyan Cat')
 clock = pygame.time.Clock()
+difficulty = 120
 
-print("press SPACE to jump")
-print("press r to restart")
-print("press q to quit")
 
+
+font = pygame.font.Font(pygame.font.get_default_font(), 36)
 
 slika_macke = pygame.image.load('Nyan-cat-slika.png').convert_alpha()
 slika_macke = pygame.transform.scale(slika_macke, (150, 100))
+
+platform_img = pygame.image.load("Klobasa.png").convert_alpha()
+platform_img = pygame.transform.scale(platform_img, (300, 50))
+
+bg = pygame.image.load("Background.jpg").convert()
+bg = pygame.transform.scale(bg, (window_width, window_height))
+
 macka = slika_macke.get_rect()
 macka.x = 200
 macka.y = 300
@@ -24,9 +30,8 @@ macka.y = 300
 velocity_y = 0
 gravity = 0.2
 jump_strength = -10
-velocity_x = -2
 
-
+velocity_x = -4
 platform_list = [
     pygame.Rect(300, 400, 300, 50),
     pygame.Rect(600, 400, 300, 50),
@@ -37,67 +42,78 @@ platform_list = [
 
 platforme_y_list = [600, 500, 400, 300, 200, 100]
 
+def start_game():
+    while True:
+        screen.fill((0, 0, 0))
+        text = font.render("Press SPACE to start | Q to quit", True, "white")
+        screen.blit(text, (250, 350))
+        pygame.display.update()
 
-platform = pygame.image.load("Klobasa.png").convert_alpha()
-platform = pygame.transform.scale(platform, (300, 50))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    pygame.mixer.init()
+                    pygame.mixer.music.load("nyan_cat_soundtrack.wav")
+                    pygame.mixer.music.play(-1)
+                    return
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    quit()
 
-
-bg = pygame.image.load("Background.jpg").convert()
-bg = pygame.transform.scale(bg, (window_width, window_height))
 
 def gravitacija():
     global velocity_y
     velocity_y += gravity
     macka.y += velocity_y
 
-def platforme_premik():
-    global velocity_y
-
-    for platform in platform_list:
-        platform.x += velocity_x
-
-
-        if macka.colliderect(platform):
-            if macka.bottom <= platform.top:
-                macka.bottom = platform.top
-                velocity_y = 0
-
 def jump():
     global velocity_y
     velocity_y = jump_strength
+
+def move_platforms():
+    for platform in platform_list:
+        platform.x += velocity_x
 
 def collisions(prev_y):
     global velocity_y
 
     for platform in platform_list:
-        platform.x += velocity_x
-
         if macka.colliderect(platform):
-            if velocity_y > 0 and prev_y + macka.height <= platform.top:
+            if velocity_y > 0 and prev_y + macka.height <= platform.top + 5:
                 macka.bottom = platform.top
                 velocity_y = 0
 
-
-    for platform in platform_list:
         if platform.x < -300:
             platform.x = window_width
             platform.y = random.choice(platforme_y_list)
 
+    if macka.bottom > window_height:
+        macka.bottom = window_height
+        velocity_y = 0
+
 def draw_every_platform():
     for rect in platform_list:
-        screen.blit(platform, rect)
+        screen.blit(platform_img, rect)
 
 def game_loop():
     global velocity_y
+    global difficulty
+
+    macka.x = 200
+    macka.y = 300
     velocity_y = 0
 
     while True:
+        difficulty = difficulty + 0.01
         prev_y = macka.y
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     jump()
@@ -107,18 +123,19 @@ def game_loop():
                 elif event.key == pygame.K_r:
                     return
 
-
         screen.blit(bg, (0, 0))
 
         gravitacija()
+        move_platforms()
         collisions(prev_y)
-        platforme_premik()
 
         screen.blit(slika_macke, macka)
         draw_every_platform()
 
         pygame.display.update()
-        clock.tick(120)
+        clock.tick(difficulty)
+
+start_game()
 
 while True:
     game_loop()
