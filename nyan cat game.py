a1a3f3fdf5907504091eebfce694dc96d85ea933
lk_07 <lukakrzan007@gmail.com>
2026-04-09 18:53:07 +0200

    Mavrica za macko

--- nyan cat game.py
+++ nyan cat game.py
@@ -3,6 +3,7 @@ import random
 
 pygame.init()
 
+#Osnovne nastavitve ekrana
 window_height = 800
 window_width = 1200
 screen = pygame.display.set_mode((window_width, window_height))
@@ -10,27 +11,45 @@ screen = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Nyan Cat')
 clock = pygame.time.Clock()
 
+#Navodila
 print("press SPACE to jump")
 print("press r to restart")
 print("press q to quit")
 
+#Nastavitve glasbe
 pygame.mixer.init()
 pygame.mixer.music.load("nyan_cat_soundtrack.wav")
 pygame.mixer.music.play(-1)
 
-
+#Nastavitve macke
 slika_macke = pygame.image.load('Nyan-cat-slika.png').convert_alpha()
 slika_macke = pygame.transform.scale(slika_macke, (150, 100))
 macka = slika_macke.get_rect()
 macka.x = 200
 macka.y = 300
 
+#Nastavitve mavrice za macko
+trail = []
+trail_length = 140
+
+barve = [
+    (255, 0, 0), #rdeca
+    (255, 127, 0), #oranzna
+    (255, 255, 0), #rumena
+    (0, 255, 0), #zelena
+    (0, 0, 255), #modra
+    (75, 0, 130), #vijolicna
+]
+
+scrol_speed = 2
+
+#Fizikalne kolicine
 velocity_y = 0
 gravity = 0.2
 jump_strength = -10
 velocity_x = -2
 
-
+#Nastavitve platform
 platform_list = [
     pygame.Rect(300, 400, 300, 50),
     pygame.Rect(600, 400, 300, 50),
@@ -41,14 +60,14 @@ platform_list = [
 
 platforme_y_list = [600, 500, 400, 300, 200, 100]
 
-
 platform = pygame.image.load("Klobasa.png").convert_alpha()
 platform = pygame.transform.scale(platform, (300, 50))
 
-
+#Nastavitve ozadja
 bg = pygame.image.load("Background.jpg").convert()
 bg = pygame.transform.scale(bg, (window_width, window_height))
 
+#Funkcije
 def gravitacija():
     global velocity_y
     velocity_y += gravity
@@ -91,6 +110,11 @@ def draw_every_platform():
     for rect in platform_list:
         screen.blit(platform, rect)
 
+def premik_trail():
+    for t in trail:
+        t[0] -= scrol_speed
+
+#Glavna zanka
 def game_loop():
     global velocity_y
     bg_x = 0
@@ -112,6 +136,7 @@ def game_loop():
                 elif event.key == pygame.K_r:
                     return
 
+        #Programiranje ozadja
         screen.fill((255, 255, 255))
         bg_x -= 2
 
@@ -120,14 +145,30 @@ def game_loop():
         if bg_x <= -bg.get_width():
             bg_x = 0
 
-
+        #Osnovni dejavniki igrice
         gravitacija()
         collisions(prev_y)
         platforme_premik()
 
+        #Mavrica
+        trail.append([macka.x + 40, macka.y + 20])
+
+        premik_trail()
+
+        if len(trail) > trail_length:
+            trail.pop(0)
+
+        for i, t in enumerate(trail):
+            for j, barva in enumerate(barve):
+                pygame.draw.rect(screen, barva, (t[0], t[1] + j*10, 40, 10))
+
+        #Macka
         screen.blit(slika_macke, macka)
+
+        #Platforme
         draw_every_platform()
 
+        #Osnovna koda
         pygame.display.update()
         clock.tick(120)
 
